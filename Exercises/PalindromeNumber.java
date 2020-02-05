67ad14fac097f943c0790bc504084529cd6362a4
shwcl <52106536+shwcl@users.noreply.github.com>
2020-02-05 19:57:13 -0400

    Create PalindromeNumber.java

---
+++ Exercises/PalindromeNumber.java
@@ -0,0 +1,22 @@
+package local.home;
+
+
+public class PalindromeNumber {
+
+
+    public static boolean isPalindrome(int number) {
+
+        String strNumber = String.valueOf(number);
+        StringBuilder sb = new StringBuilder(strNumber);
+
+        return strNumber.equals(sb.reverse().toString());
+    }
+
+
+    public static void main(String[]args) {
+
+        System.out.println(isPalindrome(123454321));
+
+    }
+
+}
