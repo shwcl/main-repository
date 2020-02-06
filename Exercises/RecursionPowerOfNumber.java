43cab409b38d6db0554f78690033ac630c969a24
Sherwin Clarke <52106536+shwcl@users.noreply.github.com>
2020-02-06 12:37:02 -0400

    Update RecursionPowerOfNumber.java

--- Exercises/RecursionPowerOfNumber.java
+++ Exercises/RecursionPowerOfNumber.java
@@ -1,1 +1,17 @@
-
+public class RecursionPowerOfNumber {
+    
+    public static int getResultFromPower(int n, int p) {
+        
+        if (p == 0) {
+            return 1;
+        } else if (p == 1) {
+            return n;
+        }
+        return getPower(n, p - 1) * n;
+    }
+    
+  
+	public static void main(String[] args) {
+		System.out.println(getResultFromPower(5,4));
+	}
+}
