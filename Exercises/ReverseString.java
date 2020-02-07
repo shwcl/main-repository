ad19ae47a587adce9ccd31187250ca69d7a51ba0
Sherwin Clarke <52106536+shwcl@users.noreply.github.com>
2020-02-07 06:26:58 -0400

    Update ReverseString.java

--- Exercises/ReverseString.java
+++ Exercises/ReverseString.java
@@ -1,22 +1,20 @@
+/* This function returns a string in reverse order */
+
 package local.home;
 
 public class ReverseString {
 
     public static String reverse(String str) {
-
+      
         StringBuilder sb = new StringBuilder();
-
+      
         for(int i= str.length() - 1; i >= 0; i--) {
             sb.append(str.charAt(i));
         }
-
         return sb.toString();
-
     }
 
     public static void main (String[] args) {
-
         System.out.println(reverse("hello world"));
-
     }
 }
