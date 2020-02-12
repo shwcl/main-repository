f56fc952a4e6bf2aad4799b753ce2db04bb07267
shwcl <52106536+shwcl@users.noreply.github.com>
2020-02-12 06:44:57 -0400

    Create mysqli_connect.php

---
+++ projects/user-registration-system-php-mysql/includes/mysqli_connect.php
@@ -0,0 +1,20 @@
+<?php 
+
+//This filecontains the database access information
+
+//This file also establishes a connection to MySQL, selects the database and sets the encoding
+
+//set the database access information as constants
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'register');
+
+//Make the connection
+$dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die('Could not connect to MySQL: ' . mysqli_connect_error());
+
+//set the encoding...
+mysqli_set_charset($dbc, 'utf8');
+	
+
+
