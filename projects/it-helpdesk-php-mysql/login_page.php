35ed87a5d5c4eab6d8339ff80443a0ba57ee3562
shwcl <52106536+shwcl@users.noreply.github.com>
2020-02-12 00:07:07 -0400

    Create login_page.php

---
+++ projects/it-helpdesk-php-mysql/login_page.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+ <title>IT Helpdesk App</title>
+ <meta charset=utf-"8">
+ <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
+</head>
+
+<body>
+
+	<div class="topline"> </div>
+		
+	<div class="header">
+		<h3>UNDP Guyana</h3>
+		<h1>IT Helpdesk</h1>
+	</div>
+
+	<div class="container">
+
+	<h4>Login Page</h4>
+
+		<form action="login_pagex.php" method="post">
+
+			<table>	
+
+		
+				<tr><td>Username:</td>
+					<td><input type="text" name="username" size="30" maxlength="30" required /></td>
+				</tr>
+
+				<tr></tr>
+
+				<tr>	
+					<td>Password:</td> 
+					<td> <input type="password" name="pwd" size="30" maxlength="30" required /></td> 
+				</tr>
+
+				<tr></tr>
+
+				<tr>
+					<td></td><td align="right"><input type="submit" name="submit" value="Submit"></td>
+				</tr>
+			</table>
+		</form>
+
+	</div>	
+
+	<div class="footer">
+		<div class="footer-text">  IT Helpdesk. All Rights Reserved.  &copy; 2018</div>
+	</div>
+
+</body>
+
+</html>
