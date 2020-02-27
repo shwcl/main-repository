a3df2288741b4848ae55978c46ad77e3303ec1c3
Sherwin Clarke <52106536+shwcl@users.noreply.github.com>
2020-02-27 01:29:50 -0400

    Update AssetDao.java

--- projects/it-hardware-inventory-java/SpringMVCProject/src/com/AssetDao.java
+++ projects/it-hardware-inventory-java/SpringMVCProject/src/com/AssetDao.java
@@ -15,27 +15,15 @@ import com.zinnia.springmvc.AssetRowMapper;
 @Repository
 public class AssetDao {
 	
-	
-	// we can use string container to instantiate the bean .. dependency???
-	
 	JdbcTemplate jdbcTemplate;
-	
-
-	//Asset asset;
-	
 	
 	@Autowired
 	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
 	    this.jdbcTemplate = jdbcTemplate;
 	}
 	
-	
 	// Method to Add an Asset
 	public int addAsset(Asset asset) {
-		
-	
-		// AssetType assetType = this.getAssetTypeObj(asset);
-		
 		
 		//insert into the database, the fields of the asset object
 		String sql = "INSERT INTO asset(make, model, asset_type_id) VALUES(?,?,?)";
@@ -46,21 +34,11 @@ public class AssetDao {
 	public int updateAsset(Asset asset) {
 		
 		int result;
-		
-	//	AssetType assetType = this.getAssetTypeObj(asset);
-	//	System.out.println("some details: " + assetType.getAssetTypeId());
-	//	System.out.println("some details: " + assetType.getAssetTypeName());
-
-
 		String sql = "UPDATE asset SET make = ?, model = ?, asset_type_id = ? WHERE asset_id = ?";
 		
 		result = jdbcTemplate.update(sql, new Object[] {asset.getMake(), asset.getModel(), asset.getAssetTypeId(), asset.getAssetId()});
 		return result;
-		
-		//jdbcTemplate.update(sql, new Object[] {"editMake", "editModel", 1, 1});
-		
 	}
-	
 	
 	// Method to Delete an Asset
 	public int deleteAsset(Asset asset) {
@@ -77,7 +55,6 @@ public class AssetDao {
   public Asset getAssetById(int assetId) throws EmptyResultDataAccessException {
 
 	  Asset asset = null;
-	  
 	  try {
 		  
 		  String sql = "SELECT asset_id, make, model, asset.asset_type_id, asset_type_desc FROM asset JOIN asset_type on asset.asset_type_id = asset_type.asset_type_id\n" +
@@ -97,7 +74,7 @@ public class AssetDao {
 	   
 	   String sql = "SELECT asset_id, make, model, asset.asset_type_id, asset_type_desc FROM asset JOIN asset_type on asset.asset_type_id = asset_type.asset_type_id";
    	   List<Asset> assets = jdbcTemplate.query(sql, new AssetRowMapper());
-      	return assets;
+      	   return assets;
    }
    
    
@@ -108,11 +85,8 @@ public class AssetDao {
 	   
 	   String sql = "SELECT asset_id, make, model, asset.asset_type_id, asset_type_desc FROM asset LEFT JOIN asset_type on asset.asset_type_id = asset_type.asset_type_id LIMIT ?, ?";
    	   List<Asset> assets = jdbcTemplate.query(sql, new Object[] {offset, recordsPerPage}, new AssetRowMapper());
-       return assets;
-      
-       // LIMIT [x], [y] 
-       // [x] - offset - at what record number to start retrieving the records from
-       // [y] - the limit which represents # of records to display per page
+           return assets;
+  
   }
    
    
@@ -122,7 +96,6 @@ public class AssetDao {
 	   
 	   String sql = "SELECT * FROM asset_type ORDER BY asset_type_desc ASC";
 	   List<AssetType> assetTypeList = jdbcTemplate.query(sql, new AssetTypeRowMapper());
-	   
 	   return assetTypeList;
    } 
 	
@@ -131,9 +104,7 @@ public class AssetDao {
   public AssetType getAssetTypeInfo(int assetTypeId) throws EmptyResultDataAccessException {
 	  
 	  AssetType assetType;
-  
-	  try {
-		  
+  	  try {
 		  String sql = "SELECT asset_type_id, asset_type_desc FROM asset_type where asset_type_id = ?";
 		  assetType = jdbcTemplate.queryForObject(sql, new Object[] {assetTypeId}, new AssetTypeRowMapper());	
 		  return assetType;
