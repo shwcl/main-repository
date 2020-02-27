97ec624b21e529ebea3ce0a0b55cbe4997933343
Sherwin Clarke <52106536+shwcl@users.noreply.github.com>
2020-02-27 01:31:05 -0400

    Update AssetTypeRowMapper.java

--- projects/it-hardware-inventory-java/SpringMVCProject/src/com/AssetTypeRowMapper.java
+++ projects/it-hardware-inventory-java/SpringMVCProject/src/com/AssetTypeRowMapper.java
@@ -2,11 +2,9 @@ package com.zinnia.springmvc;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
 import org.springframework.jdbc.core.RowMapper;
 
 public class AssetTypeRowMapper implements RowMapper<AssetType> {
-	
 	
 	@Override
 	public AssetType mapRow(ResultSet rs, int rowNum) throws SQLException  {
@@ -14,9 +12,6 @@ public class AssetTypeRowMapper implements RowMapper<AssetType> {
 		AssetType assetType = new AssetType();  
 		assetType.setAssetTypeId(rs.getInt(1));  
 		assetType.setAssetTypeName(rs.getString(2));  
-
-        return assetType;  
-		
+    		return assetType;  
 	}
-	
 }
