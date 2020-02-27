38c78ea4ffd6586687cb1994684a5e3ae9f66d48
Sherwin Clarke <52106536+shwcl@users.noreply.github.com>
2020-02-27 01:58:34 -0400

    Update MySQLDataSource.java

--- projects/it-hardware-inventory-java/SpringMVCProject/src/com/MySQLDataSource.java
+++ projects/it-hardware-inventory-java/SpringMVCProject/src/com/MySQLDataSource.java
@@ -2,26 +2,18 @@ package com.zinnia.springmvc;
 
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-
 public class MySQLDataSource {
 	
 	DriverManagerDataSource dataSource;
-	
 	public String nameString;
 	
-	
-	
-	//constructor - so when we create a new instance of this class, we have all our data source parameters defined
+	//constructor
 	public MySQLDataSource() {
 		
 		dataSource = new DriverManagerDataSource();
-		
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 		dataSource.setUrl("jdbc:mysql//localhost:3306/asset_db");
 		dataSource.setUsername("xxxx");
 		dataSource.setPassword("xxxx");
-
-
 	}
-
 }
