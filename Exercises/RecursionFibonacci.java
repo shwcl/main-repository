c6d3b2305f2691dc163c684cc66facc8112c6f02
Sherwin Clarke <52106536+shwcl@users.noreply.github.com>
2023-02-18 22:16:28 -0400

    Update RecursionFibonacci.java

--- Exercises/RecursionFibonacci.java
+++ Exercises/RecursionFibonacci.java
@@ -1,19 +1,14 @@
 public class RecursionFibonacci {
-
     public static int fibonacci(int n) {
-
         if(n == 1) {
             return 1;
         } else if (n == 2) {
             return 1;
         }
-
         return fibonacci(n-2) + fibonacci(n-1);
     }
 
     public static void main (String[] args) {
-
-        System.out.println("Fibonacci: " + fibonacci(4));
+        System.out.println("The answer is: " + fibonacci(4));
     }
-
 }
