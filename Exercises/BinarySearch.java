f137ec2ae91043c8fe251fcfb15c20336b0d0b37
Sherwin Clarke <52106536+shwcl@users.noreply.github.com>
2023-05-21 07:00:29 -0400

    Create BinarySearch.java

---
+++ Exercises/BinarySearch.java
@@ -0,0 +1,37 @@
+/* Binary Search */
+
+package SearchAlgorithms;
+
+public class BinarySearch {
+
+    public static  int search(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end){
+            int midpoint = start + (end - start)/2;
+
+            if(target == arr[midpoint]){
+                return midpoint;
+            } else if (target < arr[midpoint]){
+                end = midpoint - 1;
+            } else {
+                start = midpoint + 1;
+            }
+        }
+        
+        return - 1;
+
+    }
+
+
+    public static void main (String[] args){
+                       //0  1  2  3  4   5   6   7   8    9
+        int[] numbers = {1, 3, 5, 8, 10, 15, 78, 80, 96, 100};
+        int target = 100;
+        // target = 15
+
+        System.out.println(search(numbers, target));
+
+    }
+}
